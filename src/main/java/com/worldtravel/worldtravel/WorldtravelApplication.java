1cb081db4b136374e35d0943a84164a10fb0607a
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2026-02-08 19:39:19 +0100

    configurazione progetto

---
+++ src/main/java/com/worldtravel/worldtravel/WorldtravelApplication.java
@@ -0,0 +1,13 @@
+package com.worldtravel.worldtravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldtravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldtravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worldtravel/worldtravel/WorldtravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldtravel.worldtravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldtravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
